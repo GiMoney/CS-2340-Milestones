e3d93981736935b98ff53ecab27f2501a2678914
druzinov <druzy615@gmail.com>
2019-09-22 21:40:34 -0400

    Added spinners to modify skills

--- Deliverables/src/PlayerConfigurationView.java
+++ Deliverables/src/PlayerConfigurationView.java
@@ -34,33 +34,76 @@ public class PlayerConfigurationView extends ViewController {
         diffList.setBounds(250, 90, 200, 40);
         cp.add(diffList, BorderLayout.CENTER);
 
-        JLabel skills = new JLabel("Allocate Skill Points:");
-        skills.setBounds(20, 130, 200, 40);
+        int totalPts;
+        if (diffList.getSelectedItem().equals("Easy")) {
+            totalPts = 16;
+        } else if (diffList.getSelectedItem().equals("Medium")) {
+            totalPts = 12;
+        } else {
+            totalPts = 8;
+        }
+
+        JLabel skills = new JLabel("Allocate Skill Points: (" + totalPts + " available)");
+        skills.setBounds(20, 130, 400, 40);
         cp.add(skills, BorderLayout.CENTER);
 
         JLabel pilot = new JLabel("Pilot");
         pilot.setBounds(20, 150, 200, 40);
         cp.add(pilot, BorderLayout.CENTER);
 
+        JSpinner pSkill = new JSpinner(
+                new SpinnerNumberModel(0, 0, totalPts, 1));
+        pSkill.setBounds(250, 160, 40, 20);
+        cp.add(pSkill, BorderLayout.CENTER);
+        Integer allocatedPts = 0;
+        /*pSkill.addChangeListener(e -> {
+                allocatedPts = (Integer)((JSpinner)e.getSource()).getValue();
+        });
+        */
         JLabel fighter = new JLabel("Fighter");
         fighter.setBounds(20, 170, 200, 40);
         cp.add(fighter, BorderLayout.CENTER);
 
+        JSpinner fSkill = new JSpinner(new SpinnerNumberModel(0, 0, totalPts, 1));
+        fSkill.setBounds(250, 180, 40, 20);
+        cp.add(fSkill, BorderLayout.CENTER);
+        /*JLabel tempF = new JLabel();
+        fSkill.addChangeListener(e -> {
+            tempF.setText((String)((JSpinner)e.getSource()).getValue());
+        });
+        */
         JLabel merchant = new JLabel("Merchant");
         merchant.setBounds(20, 190, 200, 40);
         cp.add(merchant, BorderLayout.CENTER);
+
+        JSpinner mSkill = new JSpinner(new SpinnerNumberModel(0, 0, totalPts, 1));
+        mSkill.setBounds(250, 200, 40, 20);
+        cp.add(mSkill, BorderLayout.CENTER);
+        /*JLabel tempM = new JLabel();
+        mSkill.addChangeListener(e -> {
+            tempM.setText((String)((JSpinner)e.getSource()).getValue());
+        });
+        */
 
         JLabel engineer = new JLabel("Engineer");
         engineer.setBounds(20, 210, 200, 40);
         cp.add(engineer, BorderLayout.CENTER);
 
-        JLabel totalPtsLeft = new JLabel("Total Points Left:");
+        JSpinner eSkill = new JSpinner(new SpinnerNumberModel(0, 0, totalPts, 1));
+        eSkill.setBounds(250, 220, 40, 20);
+        cp.add(eSkill, BorderLayout.CENTER);
+        /*JLabel tempE = new JLabel();
+        eSkill.addChangeListener(e -> {
+            tempE.setText((String)((JSpinner)e.getSource()).getValue());
+        });
+        
+         */
+
+        JLabel totalPtsLeft = new JLabel("Total Points Left:" + (totalPts - allocatedPts));
         totalPtsLeft.setBounds(20, 230, 200, 40);
         cp.add(totalPtsLeft, BorderLayout.CENTER);
 
         JButton b = new JButton("Configure Player");
-        int bLocX = view.getWidth() / 2 - 100;
-        int bLocY = view.getHeight() - 50;
 
         b.setSize(200, 40);
         b.addActionListener(new SegueListener());

97a1c39c286ff0447311eaea0c1231179f68d5c9
brianapples <brianhaolin@gmail.com>
2019-09-22 21:39:28 -0400

    working on playerconfigview
    Point allocation for each skill

--- Deliverables/src/PlayerConfigurationView.java
+++ Deliverables/src/PlayerConfigurationView.java
@@ -9,7 +9,7 @@ public class PlayerConfigurationView extends ViewController {
     static PlayerInfoView next;
 
     public static void main(String[] args) {
-        view.setSize(500,600);
+        view.setSize(800,600);
         Container cp = view.getContentPane();
         cp.setLayout(new BorderLayout());
 
@@ -34,25 +34,62 @@ public class PlayerConfigurationView extends ViewController {
         diffList.setBounds(250, 90, 200, 40);
         cp.add(diffList, BorderLayout.CENTER);
 
-        JLabel skills = new JLabel("Allocate Skill Points:");
+        JLabel skillsnum = new JLabel("hi");
+        JButton select = new JButton("Select");
+        select.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String diff = (String) diffList.getSelectedItem();
+                System.out.print(diff);
+                if (diff.equals("Easy")) {
+                    skillsnum.setText("16");
+                } else if (diff.equals("Medium")) {
+                    skillsnum.setText("12");
+                } else if (diff.equals("Hard")) {
+                    skillsnum.setText("8");
+                }
+            }
+        });
+        select.setBounds(460, 90, 200, 40);
+        cp.add(select, BorderLayout.CENTER);
+
+
+        JLabel skills = new JLabel("Total Skill Points for that class:");
         skills.setBounds(20, 130, 200, 40);
         cp.add(skills, BorderLayout.CENTER);
+        skillsnum.setBounds(230, 130, 200, 40);
+        cp.add(skillsnum, BorderLayout.CENTER);
 
         JLabel pilot = new JLabel("Pilot");
         pilot.setBounds(20, 150, 200, 40);
         cp.add(pilot, BorderLayout.CENTER);
+        JButton addpilot = new JButton("Add");
+        int pilotpointscounter = 0;
+        JLabel pilotpoints =  new JLabel(pilotpointscounter + "");
+        addpilot.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                pilotpointscounter += 1;
+                pilotpoints.setText(pilotpointscounter + "");
+            }
+        });
+        addpilot.setBounds(150, 150, 200, 40);
+        cp.add(addpilot, BorderLayout.CENTER);
+        pilotpoints.setBounds(120, 150, 200, 40);
+        cp.add(pilotpoints, BorderLayout.CENTER);
 
         JLabel fighter = new JLabel("Fighter");
         fighter.setBounds(20, 170, 200, 40);
         cp.add(fighter, BorderLayout.CENTER);
+        JButton addFighter = new JButton("Add");
 
         JLabel merchant = new JLabel("Merchant");
         merchant.setBounds(20, 190, 200, 40);
         cp.add(merchant, BorderLayout.CENTER);
+        JButton addMerchant = new JButton("Add");
 
         JLabel engineer = new JLabel("Engineer");
         engineer.setBounds(20, 210, 200, 40);
         cp.add(engineer, BorderLayout.CENTER);
+        JButton addEngineer = new JButton("Add");
 
         JLabel totalPtsLeft = new JLabel("Total Points Left:");
         totalPtsLeft.setBounds(20, 230, 200, 40);

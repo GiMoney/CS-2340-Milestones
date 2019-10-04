c29a493d3f785add3b630b888e3d0c733848ac31
GiMoney <gsuen6@gatech.edu>
2019-10-03 20:35:32 -0400

    M4 boys
    added more functionally, may bee to be fixed

--- Deliverables/src/Universe.java
+++ Deliverables/src/Universe.java
@@ -4,28 +4,71 @@ import java.util.HashMap;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
+import java.util.List;
+import java.util.Collections;
 
 public class Universe extends Game {
 
     public Universe(String[] name){
-        ArrayList duplic = new ArrayList(10);
-        ArrayList cord = new ArrayList(10);
-        int i = 0;
-        while(i < 10){
+        List<String> duplic = new ArrayList<>(10);
+        ArrayList<TechLevel> tech = new ArrayList(10);
+        ArrayList region = new ArrayList(10);
+
+        /*while(i < 10){
             String names = name[(int)(Math.random() * 10)];
             if(!duplic.contains(names)){
-                System.out.println("ENTER");
                 duplic.add(names);
                 i++;
             }
           //  System.out.println(i);
         }
+
+        */
+        duplic = Arrays.asList(name);
+        Collections.shuffle(duplic);
+        Set<Integer> setX = new HashSet<Integer>();
+        Set<Integer> setY = new HashSet<Integer>();
+
+        Random r = new Random();
+        while(setX.size() < 10 || setY.size() < 10) {
+            int x = r.nextInt((20) + 1) - 0;
+            int y = r.nextInt((20) + 1) - 0;
+            setX.add(x);
+            setY.add(y);
+            System.out.print(setX);
+            System.out.print(setY);
+            System.out.println("SIZEX:" + setX.size());
+            System.out.println("SIZEY:" + setY.size());
+        }
+
+        Iterator itr = setX.iterator();
+        Iterator itr2 = setY.iterator();
+        int g = 0;
+        int p = 0;
+
+        while(p < 10) {
+            Random random = new Random();
+            TechLevel randomX = TechLevel.values()[random.nextInt(TechLevel.values().length)];
+            if (!tech.contains(randomX)) {
+                tech.add(randomX);
+                p++;
+            }
+        }
+
+        while(itr.hasNext() && itr2.hasNext()) {
+            region.add(new Region((Integer)itr.next() * 10, (Integer) itr2.next() * 10, tech.get(g), name[g]));
+            g++;
+            System.out.print("while2");
+        }
+
+
         //System.out.println(duplic.size());
         //for(int j =0; j < duplic.size();j++){
           //  System.out.println(duplic.get(j).toString());
        // }
        // System.out.println(duplic);
-        player.setRegion(duplic.get(0).toString());
+        player.setRegion(name[0].toString());
        // System.out.println(player.getRegion());
         //duplic.remove(0);
 
@@ -37,11 +80,12 @@ public class Universe extends Game {
 //            loc.put(duplic.get(i).toString(), "16");
 //            i++;
 //        }
-        Object[] locat = duplic.toArray();
-        super.RegionList = new JComboBox(locat);
+        //Object[] locat = name;
+        super.RegionList = new JComboBox(region.toArray());
 
 
 
 
     }
+
 }
